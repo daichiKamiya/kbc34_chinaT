6a9fb0191ca11af908587d773b9a2deab7db06e1
daichiKamiya <daichi_k-11a@stu.kbc.ac.jp>
2014-02-05 15:35:58 +0900

    kamiya accDB_search

--- src/AccessDB.java
+++ src/AccessDB.java
@@ -30,97 +30,61 @@ public class AccessDB {
         }
     }
 	
-	DefaultListModel idSelectDB(int id){
-		DefaultListModel defaultList = new DefaultListModel();
-		String resultData = new String();
+	
+	// id Search
+	DefaultTableModel idSelectDB(int id){
+		String[] clamNames = {"社員番号","名前","部署","役職"};
+		String[] sqlResult = new String[4];
+		DefaultTableModel returnTable = new DefaultTableModel(clamNames,0);
 		try {
-			//resultは書き直しが必要
-			result = statement.executeQuery("SELECT emp_id,emp_name,dept FROM temp_emp where emp_id = "+ id +" and DEL_FLG = 0");
-			 while(result.next()) {
-	               System.out.print(result.getInt(1));
-	               resultData = result.getString(1)+ result.getString(2)+ result.getString(3);
-	               defaultList.addElement(resultData);
-	           }
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return defaultList;
-	}
-
-	DefaultListModel nameSelectDB(String name){
-		DefaultListModel defaultList = new DefaultListModel();		
-		String resultData = new String();
-		try {
-			//resultは書き直しが必要
-			result = statement.executeQuery("SELECT emp_id,emp_name,dept FROM temp_emp where emp_name = '"+ name +"' and DEL_FLG = 0");
+			result = statement.executeQuery("SELECT * FROM empmanager where emp_id = "+ id +" and DEL_FLG = 0");
 			while(result.next()) {
-	               resultData = result.getString(1) + result.getString(2) + result.getString(3);
-	               defaultList.addElement(resultData);
+	           for(int i=0; i<4;i++){
+	            	 sqlResult[i] = result.getString(i+1);
+	            }	             
+	             returnTable.addRow(sqlResult);    
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return defaultList;
+		return returnTable;
 	}
 	
-	/*	
-	String nameSelectDB(String name){
-		String resultData = new String();
+	
+	// name Search
+	DefaultTableModel nameSelectDB(String name){
+		String[] clamNames = {"社員番号","名前","部署","役職"};
+		String[] sqlResult = new String[4];
+		DefaultTableModel returnTable = new DefaultTableModel(clamNames,0);
 		try {
-			//resultは書き直しが必要
-			result = statement.executeQuery("SELECT * FROM emp where ename = '"+ name +"'");
+			result = statement.executeQuery("SELECT * FROM empmanager where emp_name = '"+ name +"' and DEL_FLG = 0");
+			
+			
 			while(result.next()) {
-	               System.out.print(result.getInt(1) + "\t");
-	               System.out.print(result.getString(2) +"\t");
-	               System.out.println(result.getString(3) +"\t");
-	               resultData = result.getString(1) + result.getString(2) + result.getString(2);
-			 }
-		
+				for(int i=0; i<4;i++){
+					sqlResult[i] = result.getString(i+1);
+				}            
+            returnTable.addRow(sqlResult);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return resultData;
-	}
-*/
-	
-	
-  // 部署名をリストで戻す。	
-	DefaultListModel deptSelectDB(String dept){
-		DefaultListModel defaultList = new DefaultListModel();		
-		String resultData = new String();
-		try {
-			//resultは書き直しが必要
-			result = statement.executeQuery("SELECT * FROM temp_emp where dept = '"+ dept+"' and DEL_FLG = 0");
-			 while(result.next()) {
-//	               System.out.print(result.getInt(1));System.out.println(result.getString(2));System.out.println(result.getString(3));
-	               resultData = result.getString(1) + result.getString(2) + result.getString(3);
-	               defaultList.addElement(resultData);
-	           }
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return defaultList;
+		return returnTable;
 	}
 	
-	
-	
-	
+	// dept Search
 	DefaultTableModel deptSelectDB2(String dept){		
 		String resultData = new String();
-		String[] clamNames = {"社員番号","名前","部署"};
-		String[] sqlResult = new String[3];
+		String[] clamNames = {"社員番号","名前","部署","役職"};
+		String[] sqlResult = new String[4];
 		DefaultTableModel returnTable = new DefaultTableModel(clamNames,0);
 		
 		try {
-			//resultは書き直しが必要
-			result = statement.executeQuery("SELECT * FROM temp_emp where dept = '"+ dept+"' and DEL_FLG = 0");
-			
+			result = statement.executeQuery("SELECT emp_id,emp_name,dept_name,post FROM empmanager where post = '"+ dept +"' and DEL_FLG = 0");
 			while(result.next()) {				
-//	             System.out.print(result.getInt(1));System.out.println(result.getString(2));System.out.println(result.getString(3));
-//	             resultData = result.getString(1) + result.getString(2) + result.getString(3);
-	             for(int i=0; i<3;i++){
+				for(int i=0; i<4;i++){
 	            	 sqlResult[i] = result.getString(i+1);
-	             }
+	            }
 	             
 	             returnTable.addRow(sqlResult);
 	        }
