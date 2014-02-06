c138ce7d4c81752c055e2f473a831bcdcfdd36e9
daichiKamiya <daichi_k-11a@stu.kbc.ac.jp>
2014-02-06 09:34:42 +0900

    master_base

--- src/EmpManager.java
+++ src/EmpManager.java
@@ -1,31 +1,32 @@
-//kamiyanomira
 import javax.swing.*;
 
 import java.awt.CardLayout;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.*;
-//chinen
+
+import javax.swing.JFrame;
+
 public class EmpManager extends JFrame implements ActionListener{
 
-  JPanel cardPanel;
+// kamiya
+	
+	JPanel cardPanel;
   CardLayout layout;
   JTextField dltEmpIdTxtFld;
   JTextField dltEmpNameTxtFld;
   JTextField dltDeptTxtFld;
   JLabel Label;
-  
-  JPanel changePanels;
-	CardLayout cardLayout;
-	String responsArray;
-	DefaultListModel responsList;
-	JList resultList = new JList();
-	JScrollPane resultScrollPanel = new JScrollPane();
+  JTextField empIdTxtFld;
+  JTextField psssTxtFld;
+  JLabel titleLabel;
+private String title;
+public String cmd;
 
   public static void main(String[] args){
 	  EmpManager frame = new EmpManager();
-
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setBounds(50, 50, 650, 550);
     frame.setTitle("管理システム");
@@ -33,289 +34,258 @@ public class EmpManager extends JFrame implements ActionListener{
   }
 
   EmpManager(){
+	  ////////////カード0/////////////////////////////
 	  
-	  final AccessDB access = new AccessDB();
-	  
+	 final JPanel card0 = new JPanel();
+	    card0.setLayout(null);
+	    card0.add(new JButton("Login"));
+            // 画面の作成
+		    setTitle(title);
+		   setBounds(100, 100, 200, 250);
+		   JLabel Title1 = new JLabel("ログイン画面");
+		  Title1.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 24));
+			  Title1.setBounds(200,10,200,30);
+		    //ラベルの作成
+		   JLabel ID = new JLabel("ID:");
+		    ID.setBounds(120,104,50,20);
+		    ID.setFont(new Font("MS　ゴシック", Font.BOLD, 20));
+		     empIdTxtFld = new JTextField("", 20);
+		     empIdTxtFld.setBounds(150,100,200,30);
+		    JLabel PW = new JLabel("PW:");
+		    PW.setBounds(105,204,50,20);
+		    PW.setFont(new Font("MS　ゴシック", Font.BOLD, 20));
+		    psssTxtFld = new JTextField("", 20);
+		    psssTxtFld.setBounds(150,200,200,30);
+		    
+		    //ログイン処理
+		    final JButton loginBtn = new JButton("ログイン");
+		    
+		    loginBtn.addActionListener( new ActionListener(){
+		    	
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					 //TODO 自動生成されたメソッド・スタブ
+			    	
+					String str1 = empIdTxtFld.getText() + psssTxtFld.getText();
+					        if (str1.equals("roothimitu"))
+					            cardPanel.add(card0,"Login");
+
+					        	 
+				}
+		    });
+
+
+
+		    //ボタンの作成
+		    loginBtn.setBounds(400,350,100,30);
+		    loginBtn.addActionListener(this);
+		    
+		    titleLabel = new JLabel();
+
+		    card0.add(Title1);
+		    card0.add(empIdTxtFld);
+		    card0.add(psssTxtFld);
+		    card0.add(ID);
+		    card0.add(PW);
+		    card0.add(loginBtn);
+
+
    //////////////////// /* カード1　（トップページ） *///////////////////////////
-    JPanel card1 = new JPanel();
-    card1.add(new JButton("button"));
-        //パネルを作成
-  		card1.setLayout(null);
-  		card1.setSize(600, 500);
-  		
-  		//ボタン作成
-  		JButton addBtn = new JButton("追加");
-  		JButton searchBtn = new JButton("検索");
-  		JButton deleteBtn = new JButton("削除");
-  		
-  		//ボタンサイズと配置
-  		addBtn.setBounds(100, 100, 100, 30);
-  		searchBtn.setBounds(200, 200, 100, 30);
-  		deleteBtn.setBounds(300, 300, 100, 30);
-  		
-  		card1.add(addBtn);
-  		card1.add(searchBtn);
-  		card1.add(deleteBtn);
+			   final JPanel card1 = new JPanel();
+			    card1.add(new JButton("Button"));
+		        //パネルを作成
+		  		card1.setLayout(null);
+		  		card1.setSize(600, 500);
+		  		
+		  		//ラベル作成
+		  		JLabel ToppageLabel = new JLabel("トップページ");
+		  		card1.add(ToppageLabel);
+		  		ToppageLabel.setFont(new Font("Aril", Font.PLAIN, 50));
+		  		//ボタン作成
+		  		JButton addBtn = new JButton("追加");
+		  		addBtn.addActionListener(this);
+		  	    addBtn.setActionCommand("Insert");
+		  	    
+		  		JButton searchBtn = new JButton("検索");
+		  		searchBtn.addActionListener(this);
+		  		searchBtn.setActionCommand("Search");
+		  	    
+		  		JButton deleteBtn = new JButton("削除");
+		  		deleteBtn.addActionListener(this);
+		  		deleteBtn.setActionCommand("Delete");
+		  		
+		  		JButton logoutBtn = new JButton("ログアウト");
+		  		logoutBtn.addActionListener(this);
+		  		logoutBtn.setActionCommand("Login");
+		  		
+		  		//ボタンサイズと配置とラベル
+		  		ToppageLabel.setBounds(50,50,300,50 );
+		  		logoutBtn.setBounds(400, 50, 150, 40);
+				addBtn.setBounds(125, 150, 360, 40);
+				searchBtn.setBounds(125, 250, 360, 40);
+				deleteBtn.setBounds(125, 350, 360, 40);
+		  		
+				card1.add(ToppageLabel);
+		  		card1.add(addBtn);
+		  		card1.add(searchBtn);
+		  		card1.add(deleteBtn);
+		  		card1.add(logoutBtn);
 
     ////////////////////* カード2 (追加ページ） */////////////////////
-    JPanel card2 = new JPanel();
-    card2.setLayout(null);
-    card2.add(new JButton("button"));
-	//　Labelに文字をいれる
-	JLabel insInsertLabel = new JLabel("社員追加");
-	JLabel insEmpIdLabel  = new JLabel("ID");
-	JLabel insNameLabel   = new JLabel("名前");
-	JLabel insDeptLabel   = new JLabel("所属");
-	JLabel insPostLabel   = new JLabel("役職");
-	
-	// フォントの大きさ指定
-	insInsertLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
-	insEmpIdLabel. setFont(new Font("Aril", Font.PLAIN, 28));
-	insNameLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
-	insDeptLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
-	insPostLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
-	
-	// Labelに対応したTextFildの作成
-	final JTextField insEmpIdTxtFld = new JTextField();
-	final JTextField insNameTxtFld = new JTextField();
-	final JTextField insDeptTxtFld = new JTextField();
-//	JTextField insPostTxtFld = new JTextField();
-	
-	//Buttonの作成
-	JButton insTopBtn = new JButton("TOP");
-	JButton insertBtn = new JButton("追加");
-	insertBtn.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e){
-        	access.insertDB(Integer.parseInt(insEmpIdTxtFld.getText()),insNameTxtFld.getText(),insDeptTxtFld.getText());
-        }
-    });
-	
-	//Label,TxtFld,ButtonをPanelに挿入
-	card2.add(insInsertLabel);card2.add(insTopBtn);
-	card2.add(insEmpIdLabel); card2.add(insEmpIdTxtFld);
-	card2.add(insNameLabel); card2.add(insNameTxtFld);
-	card2.add(insDeptLabel); card2.add(insDeptTxtFld);
-//	card2.add(insPostLabel); card2.add(insPostTxtFld);
-	card2.add(insertBtn);
-	
-	//Label,TxtFld,Buttonの位置を座標で指定
-	insInsertLabel.setBounds(50,10,300,50); insTopBtn.setBounds(380,20,100,50);
-	insEmpIdLabel.setBounds(170,125,70,30); insEmpIdTxtFld.setBounds(250,125,150,30);
-	insNameLabel. setBounds(150,200,70,30); insNameTxtFld.setBounds(250,200,150,30);
-	insDeptLabel. setBounds(150,275,70,30); insDeptTxtFld.setBounds(250,275,150,30);
-//	insPostLabel. setBounds(150,350,70,30); insPostTxtFld.setBounds(250,350,150,30);
-	insertBtn.setBounds(230,410,100,30);
-	
-	
-	//Container contentPanenu = getContentPane();
+		  		JPanel card2 = new JPanel();
+		  	    card2.setLayout(null);
+		  	    card2.add(new JButton("button"));
+		  		//　Labelに文字をいれる
+		  		JLabel insInsertLabel = new JLabel("社員追加");
+		  		JLabel insEmpIdLabel  = new JLabel("ID");
+		  		JLabel insNameLabel   = new JLabel("名前");
+		  		JLabel insDeptLabel   = new JLabel("所属");
+		  		JLabel insPostLabel   = new JLabel("役職");
+		  		
+		  		// フォントの大きさ指定
+		  		insInsertLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
+		  		insEmpIdLabel. setFont(new Font("Aril", Font.PLAIN, 28));
+		  		insNameLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
+		  		insDeptLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
+		  		insPostLabel.  setFont(new Font("Aril", Font.PLAIN, 28));
+		  		
+		  		// Labelに対応したTextFildの作成
+		  		JTextField insEmpIdTxtFld = new JTextField();
+		  		JTextField insNameTxtFld = new JTextField();
+		  		JTextField insDeptTxtFld = new JTextField();
+		  		JTextField insPostTxtFld = new JTextField();
+		  		
+		  		//Buttonの作成
+		  		JButton insTopBtn = new JButton("TOP");
+		  		insTopBtn.addActionListener(this);
+		  		insTopBtn.setActionCommand("Toppage");
+
+		  		JButton insertBtn = new JButton("追加");
+		  		insertBtn.addActionListener(this);
+		  		insertBtn.setActionCommand("Inser");
+		  		
+		  		//Label,TxtFld,ButtonをPanelに挿入
+		  		card2.add(insInsertLabel);card2.add(insTopBtn);
+		  		card2.add(insEmpIdLabel); card2.add(insEmpIdTxtFld);
+		  		card2.add(insNameLabel); card2.add(insNameTxtFld);
+		  		card2.add(insDeptLabel); card2.add(insDeptTxtFld);
+		  		card2.add(insPostLabel); card2.add(insPostTxtFld);
+		  		card2.add(insertBtn);
+		  		
+		  		//Label,TxtFld,Buttonの位置を座標で指定
+		  		insInsertLabel.setBounds(50,10,300,50); insTopBtn.setBounds(380,20,100,50);
+		  		insEmpIdLabel.setBounds(170,125,70,30); insEmpIdTxtFld.setBounds(250,125,150,30);
+		  		insNameLabel. setBounds(150,200,70,30); insNameTxtFld.setBounds(250,200,150,30);
+		  		insDeptLabel. setBounds(150,275,70,30); insDeptTxtFld.setBounds(250,275,150,30);
+		  		insPostLabel. setBounds(150,350,70,30); insPostTxtFld.setBounds(250,350,150,30);
+		  		insertBtn.setBounds(230,410,100,30);
+		  		
+		  		
+		  		//Container contentPanenu = getContentPane();
 
    /////////////////* カード3　　　（削除ページ） */////////////////////
-    JPanel Deletepanel = new JPanel();
-    Deletepanel.setLayout(null);
+		  		JPanel card3 = new JPanel();
+		  	    card3.setLayout(null);
+		  	    
+		  	    //社員番号入力フィールド
+		  	    JLabel dltEmpIdLabel = new JLabel("社員ID");
+		  	    dltEmpIdLabel.setFont(new Font("Aril", Font.PLAIN, 28));
+		  	    dltEmpIdLabel.setBounds(150, 100, 100, 30);
+		  	    dltEmpIdTxtFld = new JTextField("", 20);
+		  	    dltEmpIdTxtFld.setBounds(250, 100, 150, 30);
+		  	    
+		  	    //名前入力フィールド
+		  	    JLabel dltEmpNameLabel = new JLabel("社員名");
+		  	    dltEmpNameLabel.setBounds(150, 200, 100, 30);
+		  	    dltEmpNameLabel.setFont(new Font("Aril", Font.PLAIN, 28));
+		  	    dltEmpNameTxtFld = new JTextField("", 20);
+		  	    dltEmpNameTxtFld.setBounds(250, 200, 150, 30);
+		  	    
+		  	    //部署入力フィールド
+		  	    JLabel dltDeptLabel = new JLabel("部署");
+		  	    dltDeptLabel.setFont(new Font("Aril", Font.PLAIN, 28));
+		  	    dltDeptLabel.setBounds(150, 300, 100, 30);
+		  	    dltDeptTxtFld = new JTextField("", 10);
+		  	    dltDeptTxtFld.setBounds(250, 300, 150, 30);
+		  	    
+		  	    //enterボタン作成
+		  	    JButton enterBtn = new JButton("Enter");
+		  	    enterBtn.setBounds(450, 300, 100, 30);
+		  	    enterBtn.addActionListener(this);
+		  	    enterBtn.setActionCommand("Del");
+		  	    
+		  	    //TOPボタン作成
+		  	    JButton dltTopBtn = new JButton("TOP");
+		  	    dltTopBtn.addActionListener(this);
+		  	    dltTopBtn.setActionCommand("Toppage");
+		  	    dltTopBtn.setBounds(380, 20, 100, 50);
+		  	    
+		  	    Label = new JLabel();
 
-    //社員番号入力フィールド
-    JLabel dltEmpIdLabel = new JLabel("社員ID");
-    dltEmpIdLabel.setFont(new Font("Aril", Font.PLAIN, 28));
-    dltEmpIdLabel.setBounds(150, 100, 100, 30);
-    dltEmpIdTxtFld = new JTextField("", 20);
-    dltEmpIdTxtFld.setBounds(250, 100, 150, 30);
-    
-    //名前入力フィールド
-    JLabel dltEmpNameLabel = new JLabel("社員名");
-    dltEmpNameLabel.setBounds(150, 200, 100, 30);
-    dltEmpNameLabel.setFont(new Font("Aril", Font.PLAIN, 28));
-    dltEmpNameTxtFld = new JTextField("", 20);
-    dltEmpNameTxtFld.setBounds(250, 200, 150, 30);
-    
-    //部署入力フィールド
-    JLabel dltDeptLabel = new JLabel("部署");
-    dltDeptLabel.setFont(new Font("Aril", Font.PLAIN, 28));
-    dltDeptLabel.setBounds(150, 300, 100, 30);
-    dltDeptTxtFld = new JTextField("", 10);
-    dltDeptTxtFld.setBounds(250, 300, 150, 30);
-    
-    //enterボタン作成
-    JButton enterBtn = new JButton("Enter");
-    enterBtn.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e){
-        	access.updateDB(Integer.parseInt(dltEmpIdTxtFld.getText()));
-        }
-    });
-    enterBtn.setBounds(450, 300, 100, 30);
-    
-    //TOPボタン作成
-    JButton dltTopBtn = new JButton("TOP");
-    dltTopBtn.setBounds(380, 20, 100, 50);
-    
-    enterBtn.addActionListener(this);
-    Label = new JLabel();
+		  	    //コンテンツ追加
+		  	    JLabel deleteLabel = new JLabel("削除画面");
+		  		  deleteLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
+		  		  deleteLabel.setBounds(50, 10, 300, 50);
+		  	    
+		  		  card3.add(deleteLabel);
+		  	    
+		  		  card3.add(dltEmpIdLabel);
+		  		  card3.add(dltEmpIdTxtFld);
+		  	    
+		  		  card3.add(dltEmpNameLabel);
+		  		  card3.add(dltEmpNameTxtFld);
+		  	    
+		  		  card3.add(dltDeptLabel);
+		  		  card3.add(dltDeptTxtFld);
+		  	    
+		  		  card3.add(enterBtn);
+		  		  card3.add(dltTopBtn);
 
-    //コンテンツ追加
-    JLabel deleteLabel = new JLabel("削除画面");
-	  deleteLabel.setFont(new Font("ＭＳ ゴシック", Font.BOLD, 50));
-	  deleteLabel.setBounds(50, 10, 300, 50);
-    
-	  Deletepanel.add(deleteLabel);
-    
-	  Deletepanel.add(dltEmpIdLabel);
-	  Deletepanel.add(dltEmpIdTxtFld);
-    
-	  Deletepanel.add(dltEmpNameLabel);
-	  Deletepanel.add(dltEmpNameTxtFld);
-    
-	  Deletepanel.add(dltDeptLabel);
-	  Deletepanel.add(dltDeptTxtFld);
-    
-	  Deletepanel.add(enterBtn);
-	  Deletepanel.add(dltTopBtn);
+		  		  Label.setHorizontalAlignment(JLabel.CENTER);
+		  	      Label.setVerticalAlignment(JLabel.TOP);
+		  	      
+		  	    Container contentPane1 = getContentPane();
+		  	    contentPane1.add(card3, BorderLayout.CENTER);
+		  	    contentPane1.add(Label, BorderLayout.SOUTH);
 
-	  Label.setHorizontalAlignment(JLabel.CENTER);
-      Label.setVerticalAlignment(JLabel.TOP);
-      
-    Container contentPane = getContentPane();
-    contentPane.add(Deletepanel, BorderLayout.CENTER);
-    contentPane.add(Label, BorderLayout.SOUTH);
+		  	    
     
     /////////////////* カード4　　　（検索ページ） */////////////////////
-   
-  //Search panels
-  		JPanel searchPanel = new JPanel();
-  		searchPanel.setLayout(null); // レイアウト手動
-  		
-  		JLabel searchTitle = new JLabel("社員検索");
-  		searchTitle.setFont(new Font("ＭＳ 明朝", Font.BOLD, 26));
-  		
-  		
-  		JLabel id = new JLabel("ID検索");
-  		JLabel name = new JLabel("社員名検索");
-  		JLabel dept = new JLabel("部署名検索");
-  		final JTextField idText = new JTextField();
-  		final JTextField nameText = new JTextField();
-  		final JTextField deptText = new JTextField();
-  		JButton idSearchBtn = new JButton("検索");
-  		JButton nameSearchBtn = new JButton("検索");
-  		JButton deptSearchBtn = new JButton("検索");
-  		JButton searchReturnBtn = new JButton("TOP");
-  		
-  		//layout setting
-  		searchTitle.setBounds(250,5,150,25);
-  		id.setBounds(110,100 , 70, 40);
-  		name.setBounds(110, 180 , 70, 40);
-  		dept.setBounds(110, 260 , 70, 40);
-  		idText.setBounds(220, 100 , 160, 50);
-  		nameText.setBounds(220, 180 , 160, 50);
-  		deptText.setBounds(220, 260 , 160, 50);
-  		idSearchBtn.setBounds(400,100,60,50);
-  		nameSearchBtn.setBounds(400,180,60,50);
-  		deptSearchBtn.setBounds(400,260,60,50);
-  		searchReturnBtn.setBounds(280,410,60,40);
-  		
-  		searchPanel.add(searchTitle);
-  		searchPanel.add(id);
-  		searchPanel.add(idText);
-  		searchPanel.add(name);
-  		searchPanel.add(nameText);
-  		searchPanel.add(dept);
-  		searchPanel.add(deptText);
-  		searchPanel.add(idSearchBtn);
-  		searchPanel.add(nameSearchBtn);
-  		searchPanel.add(deptSearchBtn);
-  		searchPanel.add(searchReturnBtn);
-  		
-  		
-  		//button setting
-  		idSearchBtn.addActionListener(this);
-  	    idSearchBtn.setActionCommand("resultPanel");
-  	    nameSearchBtn.addActionListener(this);
-  	    nameSearchBtn.setActionCommand("resultPanel");
-  	    deptSearchBtn.addActionListener(this);
-  	    deptSearchBtn.setActionCommand("resultPanel");
-  	    
-  	    idSearchBtn.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e){
-              	responsList = access.idSelectDB(Integer.parseInt(idText.getText()));
-              	resultList.setModel(responsList);
-  			}
-  	    });
-  	    
-  	    nameSearchBtn.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e){
-              	responsList = access.nameSelectDB(nameText.getText().toUpperCase());
-              	resultList.setModel(responsList);
-              }
-  	    });
-  	    
-  	    deptSearchBtn.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e){
-              	responsList = access.deptSelectDB(deptText.getText());
-              	resultList.setModel(responsList);
-              }
-  	    });
-  /*	  //topに戻る　未実装
-  	  		topBtn.addActionListener(new ActionListener() {
-  	              public void actionPerformed(ActionEvent e){
-  	              
-  	              }
-  	  		});
-  */	    
-  		//resultPanels
-  		JPanel resultPanel = new JPanel();
-  		resultPanel.setLayout(null);
-  		JLabel setName = new JLabel();
-  		JButton backBtn = new JButton("戻る");
-  		backBtn.setBounds(450,150,60,40);
-  		backBtn.addActionListener(this);
-  	    backBtn.setActionCommand("resultPanel");
-  	    resultList.setBounds(160,40,300,400);
-  	    resultPanel.add(setName);
-  	    resultPanel.add(resultList);
-  	    
-  		resultPanel.add(backBtn);
-    
-    
-    
     JPanel card4 = new JPanel();
 
+    
+    
+    
+    
     cardPanel = new JPanel();
     layout = new CardLayout();
     cardPanel.setLayout(layout);
-
-    cardPanel.add(card1, "button");
-    cardPanel.add(card2, "label");
-    cardPanel.add(Deletepanel, "checkbox");
-    cardPanel.add(searchPanel,"searchPanel");
-    cardPanel.add(resultPanel,"resultPanel");
-
-    /* カード移動用ボタン */
-    JButton button1 = new JButton("トップページ");
-    button1.addActionListener(this);
-    button1.setActionCommand("button");
-
-    JButton button4 = new JButton("検索");
-    button4.addActionListener(this);
-    button4.setActionCommand("searchPanel");
-    
-    JButton button2 = new JButton("追加");
-    button2.addActionListener(this);
-    button2.setActionCommand("label");
-
-    JButton button3 = new JButton("削除");
-    button3.addActionListener(this);
-    button3.setActionCommand("checkbox");
-
-    JPanel btnPanel = new JPanel();
-    btnPanel.add(button1);
-    btnPanel.add(button4);
-    btnPanel.add(button2);
-    btnPanel.add(button3);
+    cardPanel.add(card0,"Login");
+    cardPanel.add(card1,"Toppage");
+    cardPanel.add(card2, "Insert");
+    cardPanel.add(card3, "Delete");
 
     getContentPane().add(cardPanel, BorderLayout.CENTER);
-    getContentPane().add(btnPanel, BorderLayout.PAGE_END);
   }
+
+
 
   public void actionPerformed(ActionEvent e){
-    String cmd = e.getActionCommand();
+	    String cmd = e.getActionCommand();
 
-    layout.show(cardPanel, cmd);
-  }
-}
+	    layout.show(cardPanel, cmd);
+	    
+	    String daiya = e.getActionCommand();
+
+	    if (daiya.equals("Del")){
+	      JOptionPane.showMessageDialog(this, "削除完了", "削除メッセージ",
+	        JOptionPane.PLAIN_MESSAGE);
+	    }else if(daiya.equals("Inser")){
+	    	  JOptionPane.showMessageDialog(this, "社員を一人追加しました。", "追加メッセージ",
+	    		        JOptionPane.PLAIN_MESSAGE);
+	      
+	    
+	    }
+
+	  }
+	}
